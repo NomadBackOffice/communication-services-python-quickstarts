3c00eefc2f7a39e44c894de3155c15598692dbb5
HW91 <63923099+HW91@users.noreply.github.com>
2024-11-14 02:00:08 -0500

    Update send-email-inline-attachments.py

--- send-email-advanced/send-email-inline-attachments/send-email-inline-attachments.py
+++ send-email-advanced/send-email-inline-attachments/send-email-inline-attachments.py
@@ -7,9 +7,9 @@ with open("./inline-attachment.jpg", "rb") as file:
 with open("./inline-attachment.png", "rb") as file:
     png_b64encoded = base64.b64encode(file.read())
 
-connection_string = "<ACS_CONNECTION_STRING>"
-sender_address = "<SENDER_EMAIL_ADDRESS>"
-recipient_address = "<RECIPIENT_EMAIL_ADDRESS>"
+connection_string = "endpoint=https://communications-resource2.unitedstates.communication.azure.com/;accesskey=8qRf70AaLUexh50KrQ5SWmqe2T1FWc5DDh4TRKYdf904blm2q7gfJQQJ99AJACULyCpRBuouAAAAAZCStpuW"
+sender_address = "jeff@nomadbackoffice.com"
+recipient_address = "optiontrader91@gmail.com"
 
 POLLER_WAIT_TIME = 10
 
