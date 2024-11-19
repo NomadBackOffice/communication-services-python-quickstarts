e237b4b4422f6d7e65c409760e52e32b2520f48f
HW91 <63923099+HW91@users.noreply.github.com>
2024-11-19 15:54:21 -0500

    Update App.py

--- call-recording/App.py
+++ call-recording/App.py
@@ -1,3 +1,12 @@
+import os
+
+client_id = os.getenv('AZURE_CLIENT_ID')
+tenant_id = os.getenv('AZURE_TENANT_ID')
+client_secret = os.getenv('AZURE_CLIENT_SECRET')
+connection_string = os.getenv('COMMUNICATION_STRING')
+callback_url = os.getenv('CALLBACK_URL')
+
+
 from Logger import Logger
 from Controller.RecordingsController import RecordingsController
 
