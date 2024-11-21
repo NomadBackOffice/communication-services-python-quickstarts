e9292bd4e0965ea2c93ade9b48a1c1b4ba91da1d
HW91 <63923099+HW91@users.noreply.github.com>
2024-11-21 15:45:00 -0500

    Update send-sms.py

--- send-sms-quickstart/send-sms.py
+++ send-sms-quickstart/send-sms.py
@@ -4,7 +4,7 @@ from azure.communication.sms import SmsClient
 try:
     # Quickstart code goes here
 	# Create the SmsClient object which will be used to send SMS messages
-	sms_client = SmsClient.from_connection_string("<connection_string>")
+	sms_client = SmsClient.from_connection_string("COMMUNICATION_STRING")
 	
     ## Send a 1:1 SMS Message
     # calling send() with sms values
@@ -18,8 +18,8 @@ try:
     # Send a 1:N SMS Message
 	# calling send() with sms values
 	sms_responses = sms_client.send(
-    from_="<from-phone-number>",
-    to=["<to-phone-number-1>", "<to-phone-number-2>"],
+    from_="COMMUNICATION_PHONE_NUMBER",
+    to=["USER_PHONE_NUMBER"],
     message="Hello World via SMS Python",
     enable_delivery_report=True, # optional property
     tag="custom-tag") # optional property
